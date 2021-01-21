ad913573839e88d4e96fbb5885e0d1c40d648ba7
Igor <swebdesigne@gmail.com>
2021-01-21 16:15:06 +0300

    Tracker - хранилище

---
+++ src/main/java/ru/job4j/tracker/StartUI.java
@@ -0,0 +1,14 @@
+package ru.job4j.tracker;
+
+public class StartUI {
+    public static void main(String[] args) {
+        Item item = new Item();
+        item.getLocalDateAndTime();
+        Tracker tracker = new Tracker();
+        tracker.add(item);
+        item.setId(2);
+        Item num = tracker.findById(2);
+        System.out.println(num.getId());
+        item.setName("igor");
+    }
+}
---
+++ src/main/java/ru/job4j/tracker/Tracker.java
@@ -0,0 +1,60 @@
+package ru.job4j.tracker;
+
+import java.util.Arrays;
+
+public class Tracker {
+    private final Item[] items = new Item[100];
+    private int ids = 1;
+    private int size = 0;
+
+    public Item add(Item item) {
+        item.setId(ids++);
+        items[size++] = item;
+        return item;
+    }
+
+    public Item findById(int id) {
+        int index = indexOf(id);
+        return index != -1 ? items[index] : null;
+    }
+
+    public Item[] findAll() {
+        return Arrays.copyOf(items, size);
+    }
+
+    public Item[] findByName(String key) {
+        Item[] namesWithoutNull = new Item[size];
+        int index = 0;
+        for (; index < size;) {
+            Item value = items[index];
+            if (key.equals(items[index].getName())) {
+                namesWithoutNull[index] = value;
+                index++;
+            }
+        }
+        namesWithoutNull = Arrays.copyOf(namesWithoutNull, index);
+        return namesWithoutNull;
+    }
+
+    public boolean replace(int id, Item item) {
+        boolean rsl = false;
+        int index = indexOf(id);
+        if (index != -1) {
+            item.setId(id);
+            items[index] = item;
+            rsl = true;
+        }
+        return rsl;
+    }
+
+    private int indexOf(int id) {
+        int rsl = -1;
+        for (int index = 0; index < size; index++) {
+            if (items[index].getId() == id) {
+                rsl = index;
+                break;
+            }
+        }
+        return rsl;
+    }
+}
