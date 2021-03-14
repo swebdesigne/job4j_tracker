5e98cbf3f72145b3eb08fd222f6b8f0aac82a2ae
Igor <swebdesigne@gmail.com>
2021-03-14 16:59:29 +0300

    Паспорт и жители

---
+++ src/test/java/ru/job4j/PassportOfficeTest.java
@@ -0,0 +1,18 @@
+package ru.job4j;
+
+import org.junit.Test;
+import ru.job4j.collection.Citizen;
+
+import static org.hamcrest.core.Is.is;
+import static org.junit.Assert.*;
+
+public class PassportOfficeTest {
+
+    @Test
+    public void add() {
+        Citizen citizen = new Citizen("2f44a", "Petr Arsentev");
+        PassportOffice office = new PassportOffice();
+        office.add(citizen);
+        assertThat(office.get(citizen.getPassport()), is(citizen));
+    }
+}
