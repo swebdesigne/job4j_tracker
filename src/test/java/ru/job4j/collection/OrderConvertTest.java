4b39d0e345342f06b61728d6791f9e26a6f4fb1a
Igor <swebdesigne@gmail.com>
2021-03-15 16:33:11 +0300

    реобразования List в Map

---
+++ src/test/java/ru/job4j/collection/OrderConvertTest.java
@@ -0,0 +1,31 @@
+package ru.job4j.collection;
+
+import org.junit.Test;
+
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+
+import static org.hamcrest.Matchers.is;
+import static org.junit.Assert.*;
+
+public class OrderConvertTest {
+
+    @Test
+    public void whenSingleOrder() {
+        List<Order> orders = new ArrayList<>();
+        orders.add(new Order("3sfe", "Dress"));
+        HashMap<String, Order> map = OrderConvert.process(orders);
+        assertThat(map.get("3sfe"), is(new Order("3sfe", "Dress")));
+    }
+
+    @Test
+    public void whenSameOrder() {
+        List<Order> orders = new ArrayList<>();
+        orders.add(new Order("3sfe", "Dress"));
+        orders.add(new Order("4sfe", "Dress"));
+        orders.add(new Order("4sfe", "Dress"));
+        HashMap<String, Order> map = OrderConvert.process(orders);
+        assertThat(map.get("4sfe"), is(new Order("4sfe", "Dress")));
+    }
+}
