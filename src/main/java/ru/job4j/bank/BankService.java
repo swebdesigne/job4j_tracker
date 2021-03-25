ae88357cf6f2fb8a1d7e1769ff618ade9d4a9c9e
Igor <swebdesigne@gmail.com>
2021-03-25 21:36:36 +0300

    Банковские переводы

--- src/main/java/ru/job4j/bank/BankService.java
+++ src/main/java/ru/job4j/bank/BankService.java
@@ -1,40 +1,108 @@
 package ru.job4j.bank;
 
+import ru.job4j.collection.Order;
+
 import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
 
 public class BankService {
-    private Map<String, List<User>> users = new HashMap<>();
+    private Map<User, List<Account>> users = new HashMap<>();
+
+    /**
+     * Этот метод должен добавить пользователя в систему.
+     * Чтобы добавить пользователя в систему нужно использовать метод Map.put.
+     * Этот метод принимает два параметра: пользователя и список счетов.
+     * По умолчанию нужно добавить пустой список - new ArrayList<Account>().
+     * В методе должна быть проверка, что такого пользователя еще нет в системе.
+     * Если он есть, то нового добавлять не надо.
+     */
 
     public void addUser(User user) {
-        List<User> account = new ArrayList<>();
+        List<Account> account = new ArrayList<>();
         if (!users.containsKey(user.getPassport())) {
-            account.add(user);
-            users.put(user.getPassport(), account);
+            users.put(user, account);
         }
     }
+
+    /**
+     * Должен добавить новый счет к пользователю.
+     * Первоначально пользователя нужно найти по паспорту.
+     * Для этого нужно использовать метод findByPassport.
+     * После этого мы получим список всех счетов пользователя и добавим новый счет к ним.
+     * В этом методе должна быть проверка, что такого счета у пользователя еще нет.
+     */
 
     public void addAccount(String passport, Account account) {
-
+        User user = findByPassport(passport);
+        if (findByRequisite(passport, account.getRequisite()) == null) {
+            users.get(user).add(account);
+        }
     }
+
+    /**
+     * Этот метод ищет пользователя по номеру паспорта.
+     * Здесь нужно использовать перебор всех элементов через цикл for-earch и метод Map.keySet.
+     */
 
     public User findByPassport(String passport) {
-        for (String value : users.keySet()) {
-            if (value.equals(passport)) {
+        User result = null;
+        for (User value : users.keySet()) {
+            if (value.getPassport().equals(passport)) {
+                result = value;
             }
         }
-        return users.get(passport);
+        return result;
     }
 
+    /**
+     * Этот метод ищет счет пользователя по реквизитам. Сначала нужно найти пользователя.
+     * Потом получить список счетов этого пользователя и в нем найти нужный счет.
+     */
+
     public Account findByRequisite(String passport, String requisite) {
-        return null;
+        Account result = null;
+        User user = findByPassport(passport);
+        if (user != null) {
+            for (User value : users.keySet()) {
+                for (Account list : users.get(value)) {
+                    if (list.getRequisite().equals(requisite)) {
+                        result = list;
+                        break;
+                    }
+                }
+            }
+        }
+        return result;
     }
+
+    /**
+     * Метод для перечисления денег с одного счёта на другой счёт.
+     * Если счёт не найден или не хватает денег
+     * на счёте srcAccount (с которого переводят), то метод должен вернуть false.
+     *
+     * Метод putIfAbsent позволяет проверить, если ли такой ключ в карте или нет и если его нет,
+     * то произвести вставку данных.
+     * Этот метод позволяет уменьшить количество кода.
+     *
+     * Метод List.indexOf позволяет найти индекс элемента по значению.
+     * Проверка элементов в этом методе происходит по методу equals.
+     * Обратите внимание, что в моделях User и Account используется
+     * только одно поле passport и requisite для сравнения объектов.
+     * Это позволяет использовать эти методы, без информации о всех полях.
+     */
 
     public boolean transferMoney(String srcPassport, String srcRequisite,
                                  String destPassport, String destRequisite, double amount) {
         boolean rsl = false;
+        Account account1 = findByRequisite(srcPassport, srcRequisite);
+        Account account2 = findByRequisite(destPassport, destRequisite);
+        if (account1.getBalance() >= amount) {
+            account1.setBalance(account1.getBalance() + 150);
+            account2.setBalance(account2.getBalance() + 150);
+            rsl = true;
+        }
         return rsl;
     }
 
--- src/test/java/ru/job4j/bank/BankServiceTest.java
+++ src/test/java/ru/job4j/bank/BankServiceTest.java
@@ -16,18 +16,32 @@ public class BankServiceTest {
     }
 
     @Test
+    public void whenEnterInvalidPassport() {
+        User user = new User("3434", "Petr Arsentev");
+        BankService bank = new BankService();
+        bank.addUser(user);
+        bank.addAccount(user.getPassport(), new Account("5546", 150D));
+        assertNull(bank.findByRequisite("34", "5546"));
+    }
+
+    @Test
     public void addAccount() {
-    }
-
-    @Test
-    public void findByPassport() {
-    }
-
-    @Test
-    public void findByRequisite() {
+        User user = new User("3434", "Petr Arsentev");
+        BankService bank = new BankService();
+        bank.addUser(user);
+        bank.addAccount(user.getPassport(), new Account("5546", 150D));
+        assertThat(bank.findByRequisite("3434", "5546").getBalance(), is(150D));
     }
 
     @Test
     public void transferMoney() {
+        User user = new User("3434", "Petr Arsentev");
+        BankService bank = new BankService();
+        bank.addUser(user);
+        bank.addAccount(user.getPassport(), new Account("5546", 150D));
+        bank.addAccount(user.getPassport(), new Account("113", 50D));
+        bank.transferMoney(user.getPassport(), "5546", user.getPassport(), "113", 150D);
+        assertThat(bank.findByRequisite(user.getPassport(), "113").getBalance(), is(200D));
     }
+
 }
