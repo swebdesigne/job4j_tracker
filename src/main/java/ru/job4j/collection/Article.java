f5830e3630994961f6669e5cbb43872b507ad9f2
Igor <swebdesigne@gmail.com>
2021-04-11 17:40:07 +0300

    Генератор текста

--- src/main/java/ru/job4j/collection/Article.java
+++ src/main/java/ru/job4j/collection/Article.java
@@ -1,22 +1,28 @@
 package ru.job4j.collection;
 
+import java.util.HashMap;
 import java.util.HashSet;
 
 public class Article {
     public static boolean generateBy(String origin, String line) {
-        boolean rsl = true;
-        String[] originTest = origin.replaceAll("[,.;!-]", "").split(" ");
-        String[] text = line.split(" ");
-        HashSet<String> check = new HashSet<>();
-        for (String str : originTest) {
-            check.add(str.trim());
+        boolean result = true;
+        String[] left = origin.replaceAll("\\p{P}", "").split(" ");
+        String[] right = line.replaceAll("\\p{P}", "").split(" ");
+        HashMap<String, Integer> check = new HashMap<>();
+        for (String word : left) {
+            if (check.containsKey(word)) {
+                check.put(word, check.get(word) + 1);
+            } else {
+                check.put(word, 1);
+            }
         }
-        for (String txt : text) {
-            if (!check.contains(txt)) {
-                rsl = false;
+        for (String word : right) {
+            // if (check.get(word) == 0)
+            if (!check.containsKey(word)) {
+                result = false;
                 break;
             }
         }
-        return rsl;
+        return result;
     }
 }
