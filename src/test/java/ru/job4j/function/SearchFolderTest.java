f3f29262f2d8363b50cd1398d8d8f7f6780fe64f
Igor <swebdesigne@gmail.com>
2021-04-15 15:57:08 +0300

    Встроенные функциональные интерфейсы

---
+++ src/test/java/ru/job4j/function/SearchFolderTest.java
@@ -0,0 +1,41 @@
+package ru.job4j.function;
+
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+import java.util.List;
+import java.util.function.Predicate;
+import static org.hamcrest.core.Is.*;
+
+public class SearchFolderTest {
+
+    @Test
+    public void filterSize() {
+        List<Folder> list = List.of(
+                new Folder("fix", 110),
+                new Folder("bug", 75),
+                new Folder("bug", 90)
+        );
+        List<Folder> expected = List.of(new Folder("Fix", 110));
+        Predicate<Folder> pred = f -> f.getSize() > 100;
+        System.out.println(pred);
+        List<Folder> rsl = SearchFolder.filter(list, pred);
+        assertThat(rsl, is(expected));
+    }
+
+    @Test
+    public void filterName() {
+        List<Folder> list = List.of(
+                new Folder("fix", 110),
+                new Folder("bug", 75),
+                new Folder("bug", 90)
+        );
+        List<Folder> expected = List.of(
+                new Folder("bug", 75),
+                new Folder("bug", 90)
+        );
+        Predicate<Folder> pred = f -> f.getName().contains("bug");
+        List<Folder> rsl = SearchFolder.filter(list, pred);
+        assertThat(rsl, is(expected));
+    }
+}
