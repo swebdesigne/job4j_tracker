103e27ed2ae1bc247cccb29fe3e3cb2875ca91df
Igor <swebdesigne@gmail.com>
2021-04-19 12:44:26 +0300

    Фильтрация учеников

---
+++ src/main/java/ru/job4j/function/School.java
@@ -0,0 +1,14 @@
+package ru.job4j.function;
+
+import java.util.List;
+import java.util.function.Predicate;
+import java.util.stream.Collectors;
+
+public class School {
+    public List<Student> collect(List<Student> students, Predicate<Student> predicate) {
+        return students.stream()
+                .filter(student -> predicate.test(student))
+                .collect(Collectors.toList()
+                );
+    }
+}
