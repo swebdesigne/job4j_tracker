09449587f2928b2acd7136c57cd49aaa4f1877cb
Igor <swebdesigne@gmail.com>
2021-04-21 15:23:02 +0300

    Уникальность элементов и сортировка.

--- src/main/java/ru/job4j/function/Profiles.java
+++ src/main/java/ru/job4j/function/Profiles.java
@@ -1,11 +1,14 @@
 package ru.job4j.function;
 
+import java.util.Comparator;
 import java.util.List;
 import java.util.stream.Collectors;
 
 public class Profiles {
     public List<Address> collect(List<Profile> profiles) {
         return profiles.stream().map(value -> value.getAddress())
+                .sorted(Comparator.comparing(value -> value.getCity()))
+                .distinct()
                 .collect(Collectors.toList());
     }
 }
--- src/test/java/ru/job4j/function/ProfilesTest.java
+++ src/test/java/ru/job4j/function/ProfilesTest.java
@@ -16,6 +16,7 @@ public class ProfilesTest {
         List<Profile> pr = List.of(
             new Profile(new Address("Дубна", "Энтузиастов", 12, 1)),
             new Profile(new Address("Луховицы", "Боголюбова", 1, 33)),
+            new Profile(new Address("Луховицы", "Боголюбова", 1, 33)),
             new Profile(new Address("Кимры", "Павлова", 29, 21))
         );
         List<Address> rsl = new Profiles().collect(pr);
@@ -23,6 +24,23 @@ public class ProfilesTest {
                new Address("Дубна", "Энтузиастов", 12, 1),
                new Address("Луховицы", "Боголюбова", 1, 33),
                new Address("Кимры", "Павлова", 29, 21)
+        );
+        assertThat(rsl, is(expected));
+    }
+
+    @Test
+    public void whenDeleteDuplicate() {
+        List<Profile> pr = List.of(
+                new Profile(new Address("Дубна", "Энтузиастов", 12, 1)),
+                new Profile(new Address("Луховицы", "Боголюбова", 1, 33)),
+                new Profile(new Address("Луховицы", "Боголюбова", 1, 33)),
+                new Profile(new Address("Кимры", "Павлова", 29, 21))
+        );
+        List<Address> rsl = new Profiles().collect(pr);
+        List<Address> expected = Arrays.asList(
+                new Address("Дубна", "Энтузиастов", 12, 1),
+                new Address("Кимры", "Павлова", 29, 21),
+                new Address("Луховицы", "Боголюбова", 1, 33)
         );
         assertThat(rsl, is(expected));
     }
